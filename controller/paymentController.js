27167b49a5546ad21057b3a0e10d6323b739697a
Md ashiquzzaman <ahmedashik18k@gmail.com>
2024-01-24 20:11:15 +0600

    Middleware added for token generate

--- controller/paymentController.js
+++ controller/paymentController.js
@@ -1,6 +1,15 @@
+const axios = require("axios");
+const globals = require("node-global-storage");
+
 class paymentController {
   payment_create = async (req, res) => {
-    console.log(req.body);
+    const { amount } = req.body;
+    try {
+      const { data } = await axios.post(
+        process.env.bkash_create_payment_url,
+        {}
+      );
+    } catch (error) {}
   };
 }
 module.exports = new paymentController();
